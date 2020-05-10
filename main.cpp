b39919544b4f8d066f8a9f2208b2a034ba374d27
staaahn <38550876+staaahn@users.noreply.github.com>
2020-05-10 15:24:36 -0700

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
-#include "itkImage.h"
+#include <stdio.h>
+#include <string.h>
+using namespace std;
 
-int
-main(int, char *[])
-{
-  using ImageType = itk::Image<unsigned short, 3>;
-  ImageType::Pointer image = ImageType::New();
 
-  std::cout << "ITK Hello World!" << std::endl;
-  std::cout << image << std::endl;
+void main( int argc, char *argv[] ) {
+    while (input != 0) {
+    cout << "Please select an option: \n"
+         << "1. Perform WatershedSegmentation\n" 
+         << "EXIT: 0" << endl;
+    cin >> choice;
+    cout << endl;
 
-  return EXIT_SUCCESS;
+    if(input == 1){
+        cout << "You selected Watershed Segmentation" << endl << "Working..." << endl;
+        cout << "Please fill the following fields in order: Input Image, Output Image, Lower Threshold, Output Scale Level" << endl;
+        cin >> inputImage >> outputImage >> lowerThreshold >> outputScaleLevel;
+
+        WatershedSegmentation2(inputImage.c_str(), outputImage.c_str(), lowerThreshold.c_str(), outputScaleLevel.c_str());
+    }
+  }
 }
