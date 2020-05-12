85fcc5210c19558a6f7927ad8b7fb90b83aa1818
staaahn <38550876+staaahn@users.noreply.github.com>
2020-05-11 20:59:32 -0700

    Update header.h

--- header.h
+++ header.h
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-#include "WatershedSegmentation2.cxx"
-#include "BSplineRegistration.cxx"
 using namespace std;
 
 void WatershedSegmentation2(const char* input_image, const char* output_image, const char* lower_threshold, const char* output_scalelevel );
