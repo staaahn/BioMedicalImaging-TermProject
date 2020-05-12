bdf8373af82742b003dfac4d666cd863f73bc9d4
staaahn <38550876+staaahn@users.noreply.github.com>
2020-05-11 21:24:54 -0700

    ADDED image processing functions to header

--- header.h
+++ header.h
@@ -4,3 +4,5 @@ using namespace std;
 
 void WatershedSegmentation2(const char* input_image, const char* output_image, const char* lower_threshold, const char* output_scalelevel );
 void BSplineRegistration(const char* fixedImageFile, const char* movingImageFile, const char* outputImageFile);
+void ImageReadDicomSeriesWrite(const char * input, const char * output);
+void ImageReadImageSeriesWrite(const char * input, const char * output, const char * extension);
