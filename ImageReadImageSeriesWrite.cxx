899655b54255780aadcf67063d2f4284ecc2f34d
wiamBoumaazi <54960905+wiamBoumaazi@users.noreply.github.com>
2020-05-11 21:13:10 -0700

    Update ImageReadImageSeriesWrite.cxx

--- ImageReadImageSeriesWrite.cxx
+++ ImageReadImageSeriesWrite.cxx
@@ -4,19 +4,16 @@
 #include "itkImageFileReader.h"
 #include "itkImageSeriesWriter.h"
 #include "itkNumericSeriesFileNames.h"
+#include "header.h"
  
 int
-main(int argc, char * argv[])
-{
-  if (argc < 4)
-  {
+main(int argc, char * argv[]){
+  if (argc < 4){
     std::cerr
       << "Usage: ImageReadImageSeriesWrite inputFile outputPrefix outputExtension"
       << std::endl;
     return EXIT_FAILURE;
   }
- 
-
   using ImageType = itk::Image<unsigned char, 3>;
   using ReaderType = itk::ImageFileReader<ImageType>;
   
@@ -24,39 +21,31 @@ main(int argc, char * argv[])
   reader->SetFileName(argv[1]);
   
   using Image2DType = itk::Image<unsigned char, 2>;
- 
   using WriterType = itk::ImageSeriesWriter<ImageType, Image2DType>;
- 
+  
   WriterType::Pointer writer = WriterType::New();
- 
   writer->SetInput(reader->GetOutput());
   
   using NameGeneratorType = itk::NumericSeriesFileNames;
- 
   NameGeneratorType::Pointer nameGenerator = NameGeneratorType::New();
 
   std::string format = argv[2];
   format += "%03d.";
-  format += argv[3]; // filename extension
- 
+  format += argv[3]; 
   nameGenerator->SetSeriesFormat(format.c_str());
   
-  try
-  {
+  try{
     reader->Update();
   }
-  catch (const itk::ExceptionObject & excp)
-  {
+  catch (const itk::ExceptionObject & excp){
     std::cerr << "Exception thrown while reading the image" << std::endl;
     std::cerr << excp << std::endl;
   }
-  // Software Guide : EndCodeSnippet
- 
- 
+   
   ImageType::ConstPointer inputImage = reader->GetOutput();
-  ImageType::RegionType   region = inputImage->GetLargestPossibleRegion();
-  ImageType::IndexType    start = region.GetIndex();
-  ImageType::SizeType     size = region.GetSize();
+  ImageType::RegionType region = inputImage->GetLargestPossibleRegion();
+  ImageType::IndexType start = region.GetIndex();
+  ImageType::SizeType size = region.GetSize();
   
   const unsigned int firstSlice = start[2];
   const unsigned int lastSlice = start[2] + size[2] - 1;
@@ -67,12 +56,10 @@ main(int argc, char * argv[])
   
   writer->SetFileNames(nameGenerator->GetFileNames());
  
-  try
-  {
+  try{
     writer->Update();
   }
-  catch (const itk::ExceptionObject & excp)
-  {
+  catch (const itk::ExceptionObject & excp){
     std::cerr << "Exception thrown while reading the image" << std::endl;
     std::cerr << excp << std::endl;
   }
